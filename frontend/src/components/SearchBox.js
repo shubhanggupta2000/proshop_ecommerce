7484102236bfacc2d38ba815688d71c3bdbf997a
Shubhang Gupta <guptashubhang2000@gmail.com>
2025-01-16 11:35:37 +0530

    header update

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -31,7 +31,7 @@ const SearchBox = () => {
         placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2 my-3">
+      <Button type="submit" variant="outline-success" className="p-2 my-1">
         Search
       </Button>
     </Form>
